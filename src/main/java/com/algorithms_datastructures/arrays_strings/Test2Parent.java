90c5316319e0339bf9539bfc36c734be1cbafd66
kishoremarojuu <kishoremarojuu@gmail.com>
2020-03-07 15:46:14 -0600

    src/main/java/com/algorithms_datastructures/aaa_IteratorsOnDataStructures.java

---
+++ src/main/java/com/algorithms_datastructures/arrays_strings/Test2Parent.java
@@ -0,0 +1,13 @@
+package com.algorithms_datastructures.arrays_strings;
+
+public class Test2Parent {
+
+
+
+    public String printMyName(){
+        System.out.println("Hi,wwlecome");
+        return "welcome";
+    }
+
+
+}

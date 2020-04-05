1b5a2f1aecd1d58424a6683878faee381ade0ec1
kishoremarojuu <kishoremarojuu@gmail.com>
2020-04-05 18:55:21 -0500

    Added array programms

--- src/main/java/com/algorithms_datastructures/arrays_strings/Arrays_123_FirstNonRepeatingCharacterInString.java
+++ src/main/java/com/algorithms_datastructures/arrays_strings/Arrays_123_FirstNonRepeatingCharacterInString.java
@@ -4,49 +4,98 @@ import java.net.BindException;
 import java.util.Arrays;
 
 public class Arrays_123_FirstNonRepeatingCharacterInString {
-    public static Character getkthrepeatedCharacter(String string) {
-        int k = 3;
-        if (string == null || string.length() == 0) {
-            // return null;
+    public static Character getFirstNonRepeatingCharacterLinearOptimized(String string) {
+        if(string == null || string.length() == 0) {
+            return null;
         }
 
         int n = string.length();
-        if (n == 1) {
+        if(n == 1) {
             return string.charAt(0);
         }
 
-        int[] charIdx = new int[256];
-        int[] index = new int[256];
-        // Index of non repeating characters. If repeating, then index = -2
+        int[] charIdx = new int[256];   // Index of non repeating characters. If repeating, then index = -2
         // Initialize character index of all characters to -1
-        for (int i = 0; i < 256; i++) {
+        for(int i = 0; i < 256; i++) {
             charIdx[i] = -1;
-            index[i] = n;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (charIdx[string.charAt(i)] == -1) {
+        for(int i = 0; i < n; i++) {
+            if(charIdx[string.charAt(i)] == -1) {
                 // character seen first time
                 charIdx[string.charAt(i)] = i;
-                index[string.charAt(i)] = i;
             } else {
                 // Repeated character
                 charIdx[string.charAt(i)] = -2;
-                index[string.charAt(i)] = n;
             }
         }
 
-        Arrays.sort(index);
-        if (index[k - 1] != n) {
-            return string.charAt(index[k - 1]);
+        int minIdx = n; // Index of first non repeating character
+        for(int i = 0; i < 256; i++) {
+            if(charIdx[i] >= 0 && minIdx > charIdx[i]) {
+                minIdx = charIdx[i];
+                // break;
+            }
+        }
+        return string.charAt(minIdx);
+        //return (minIdx >= 0 && minIdx < n) ? string.charAt(minIdx) : null;     //original is this, I added above line and line noO: 34 as well to ease
+    }
+
+    //second approach - not efficient
+    public static Character getFirstNonRepeatingCharacterLinear(String string) {
+        if(string == null || string.length() == 0) {
+            return null;
+        }
+
+        int n = string.length();
+        if(n == 1) {
+            return string.charAt(0);
+        }
+
+        int[] charCounts = new int[256];
+        // Initialize character counts of all characters to 0
+        for(int i = 0; i < 256; i++) {
+            charCounts[i] = 0;
+        }
+
+        for(int i = 0; i < n; i++) {
+            charCounts[string.charAt(i)]++;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(charCounts[string.charAt(i)] == 1) {
+                return string.charAt(i);
+            }
         }
         return null;
     }
 
+    //approach-3 naive method
+    public static Character getFirstNonRepeatingCharacterNaive(String string) {
+        if(string == null || string.length() == 0) {
+            return null;
+        }
+
+        int n = string.length();
+        for(int i = 0; i < n; i++) {
+            boolean flag = true;
+            for(int j = 0; j < n; j++) {
+                if(i != j && string.charAt(i) == string.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                return string.charAt(i);
+            }
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
-     //   String string = "ADBCGHIEFKJLADTVDERFSWVGHQWCNOPENSMSJWIERTFB";
-        String string = "ABABHKF";
-        System.out.println("Output: " + getkthrepeatedCharacter(string));
+        //   String string = "ADBCGHIEFKJLADTVDERFSWVGHQWCNOPENSMSJWIERTFB";
+        String string = "ABABHK";
+        //   System.out.println("Output: " + getFirstNonRepeatingCharacterNaive(string));
+        System.out.println("Output: " + getFirstNonRepeatingCharacterLinearOptimized(string));
     }
 }
--- src/main/java/com/algorithms_datastructures/arrays_strings/Arrays_123_KthNonRepeatingCharacterInString.java
+++ src/main/java/com/algorithms_datastructures/arrays_strings/Arrays_123_KthNonRepeatingCharacterInString.java
@@ -3,74 +3,50 @@ package com.algorithms_datastructures.arrays_strings;
 import java.util.Arrays;
 
 public class Arrays_123_KthNonRepeatingCharacterInString {
+    public static Character getkthrepeatedCharacter(String string, int k) {
 
-        public static int MAX_CHAR = 256;
-
-        // Returns index of k'th non-repeating character in
-        // given string str[]
-        static int kthNonRepeating(String str, int k)
-        {
-            int n = str.length();
-
-            // count[x] is going to store count of
-            // character 'x' in str. If x is not present,
-            // then it is going to store 0.
-            int[] count = new int[MAX_CHAR];
-
-            // index[x] is going to store index of character
-            // 'x' in str. If x is not present or x is
-            // repeating, then it is going to store a value
-            // (for example, length of string) that cannot be
-            // a valid index in str[]
-            int[] index = new int[MAX_CHAR];
-
-            // Initialize counts of all characters and indexes
-            // of non-repeating characters.
-            for (int i = 0; i < MAX_CHAR; i++)
-            {
-                count[i] = 0;
-                index[i] = n; // A value more than any index
-                // in str[]
-            }
-
-            // Traverse the input string
-            for (int i = 0; i < n; i++)
-            {
-                // Find current character and increment its
-                // count
-                char x = str.charAt(i);
-                ++count[x];
-
-                // If this is first occurrence, then set value
-                // in index as index of it.
-                if (count[x] == 1)
-                    index[x] = i;
-
-                // If character repeats, then remove it from
-                // index[]
-                if (count[x] == 2)
-                    index[x] = n;
-            }
-
-            // Sort index[] in increasing order. This step
-            // takes O(1) time as size of index is 256 only
-            Arrays.sort(index);
-
-            // After sorting, if index[k-1] is value, then
-            // return it, else return -1.
-            return (index[k-1] != n)? index[k-1] : -1;
+        if (string == null || string.length() == 0) {
+            // return null;
         }
 
-        // driver program
-        public static void main (String[] args)
-        {
-            String str = "ABABHKF";
-            int k = 3;
-            int res = kthNonRepeating(str, k);
-
-            System.out.println(res == -1 ? "There are less than k non-repeating characters" :
-                    "k'th non-repeating character is " + str.charAt(res));
+        int n = string.length();
+        if (n == 1) {
+            return string.charAt(0);
         }
+
+        int[] charIdx = new int[256];
+        int[] index = new int[256];
+        // Index of non repeating characters. If repeating, then index = -2
+        // Initialize character index of all characters to -1
+        for (int i = 0; i < 256; i++) {
+            charIdx[i] = -1;
+            index[i] = n;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (charIdx[string.charAt(i)] == -1) {
+                // character seen first time
+                charIdx[string.charAt(i)] = i;
+                index[string.charAt(i)] = i;
+            } else {
+                // Repeated character
+                charIdx[string.charAt(i)] = -2;
+                index[string.charAt(i)] = n;
+            }
+        }
+
+        Arrays.sort(index);
+        if (index[k - 1] != n) {
+            return string.charAt(index[k - 1]);
+        }
+        return null;
     }
 
-// Contributed by Pramod Kumar
+
+    public static void main(String[] args) {
+        //   String string = "ADBCGHIEFKJLADTVDERFSWVGHQWCNOPENSMSJWIERTFB";
+        String string = "ABABHKF";
+        int k =3;
+        System.out.println("Output: " + getkthrepeatedCharacter(string,k));
+    }
+}
